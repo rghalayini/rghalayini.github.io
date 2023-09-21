cf5ee919a8e2316cfb7b8b31ea35c3808d66ff05
Remy Ghalayini <remyghalayini@gmail.com>
2023-09-21 22:20:33 +0200

    added name description

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TopMenu from "../components/TopMenu";
+import Name from "../components/Name";
 
 const LandingPage = () => {
   return (
     <>
       <TopMenu />
+      <Name />
     </>
   );
 };
