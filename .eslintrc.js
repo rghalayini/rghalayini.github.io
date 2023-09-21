7d92605411989364fe416484f344c6f14d56d787
Remy Ghalayini <remyghalayini@gmail.com>
2023-09-21 11:25:03 +0200

    installed react and all packages

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb/hooks",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  return <div>hello world</div>;
+};
+
+export default App;
