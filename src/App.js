91d31f324faaafa59cc145b082caf4427d12e42d
Remy Ghalayini <remyghalayini@gmail.com>
2023-10-02 01:57:52 +0200

    fixed router

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import GlobheApp from "./pages/GlobheApp";
 import Resume from "./pages/Resume";
-import Index from "./pages/about/index";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
-        <Route path="/about" element={<Index />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/globhe-app" element={<GlobheApp />} />
         <Route path="/my-resume" element={<Resume />} />
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import theme from "./theme";
 import { ThemeProvider } from "@mui/material/styles";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <HashRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </HashRouter>
   </React.StrictMode>,
 );
 
