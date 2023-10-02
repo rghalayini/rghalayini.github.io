461b0af7cc347cd48917ed5f41c15bc0ba92d2a3
Remy Ghalayini <remyghalayini@gmail.com>
2023-10-02 16:08:18 +0200

    added tag manager

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import About from "./pages/About";
 import GlobheApp from "./pages/GlobheApp";
 import Resume from "./pages/Resume";
 import ReactGA from "react-ga";
+import TagManager from "react-gtm-module";
 
 const App = () => {
   useEffect(() => {
     // Initialize Google Analytics with your Tracking ID
     ReactGA.initialize("UA-180566476-1");
     ReactGA.pageview(window.location.pathname + window.location.search);
+    TagManager.initialize({
+      gtmId: "G-M3XXNGRW2H",
+    });
   }, []);
 
   return (
