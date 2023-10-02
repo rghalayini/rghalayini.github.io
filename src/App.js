ac4a143293bde91afc450c79f0ad265a25d406df
Remy Ghalayini <remyghalayini@gmail.com>
2023-10-02 02:06:01 +0200

    fixed router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import LandingPage from "./pages/LandingPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import GlobheApp from "./pages/GlobheApp";
@@ -8,7 +8,7 @@ import Resume from "./pages/Resume";
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/about" element={<About />} />
@@ -16,7 +16,7 @@ const App = () => {
         <Route path="/globhe-app" element={<GlobheApp />} />
         <Route path="/my-resume" element={<Resume />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 export default App;
