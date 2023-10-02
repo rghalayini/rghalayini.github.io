a7b6293bbd5a992f55df3f6443860a687ec0f20c
Remy Ghalayini <remyghalayini@gmail.com>
2023-10-02 02:08:33 +0200

    fixed router

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import theme from "./theme";
 import { ThemeProvider } from "@mui/material/styles";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </HashRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
