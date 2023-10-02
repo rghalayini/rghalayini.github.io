e543fb30a72ece66a7868dd711fd3e7b8d3bbd04
Remy Ghalayini <remyghalayini@gmail.com>
2023-10-02 08:18:43 +0200

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LandingPage from "./pages/LandingPage";
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import GlobheApp from "./pages/GlobheApp";
 import Resume from "./pages/Resume";
+import ReactGA from "react-ga";
 
 const App = () => {
+  useEffect(() => {
+    // Initialize Google Analytics with your Tracking ID
+    ReactGA.initialize("UA-180566476-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <HashRouter>
       <Routes>
